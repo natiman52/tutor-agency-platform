ff598b8839c47d4e569b17189c3cd4665c14fb8f
Sadorra <rodiawg@gmail.com>
2026-02-19 22:29:29 +0300

    refactor: Wrap whole app in QueryClientProvider (QueryProvider)

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,8 +1,14 @@
+// src/providers/QueryProvider.tsx
+import React, { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-export const QueryProvider = ({ children }) => (
+interface Props {
+  children: ReactNode;
+}
+
+export const QueryProvider: React.FC<Props> = ({ children }) => (
   <QueryClientProvider client={queryClient}>
     {children}
   </QueryClientProvider>
